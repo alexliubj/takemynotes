56a7f1ddae11df779a6ffb3cc822dc6a9a0fe1d3
alex <alexliubo@gmail.com>
2014-02-12 19:32:49 -0500

    Delete Tabs error

--- Src/Client/src/ca/techguys/takemynotes/view/SubCategoryActivity.java
+++ Src/Client/src/ca/techguys/takemynotes/view/SubCategoryActivity.java
@@ -9,6 +9,7 @@ import ca.techguys.takemynotes.R.menu;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -30,54 +31,6 @@ public class SubCategoryActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sub_category);
-		
-		//add by atom
-		tabHost = getTabHost();
-		tabHost.setOnTabChangedListener(this);
-
-		// setup list view 1
-		listView1 = (ListView) findViewById(R.id.list1);
-
-		// create some dummy strings to add to the list
-		List<String> list1Strings = new ArrayList<String>();
-		list1Strings.add("Item 1");
-		list1Strings.add("Item 2");
-		list1Strings.add("Item 3");
-		list1Strings.add("Item 4");
-		listView1.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, list1Strings));
-
-		// setup list view 2
-		listView2 = (ListView) findViewById(R.id.list2);
-
-		// create some dummy strings to add to the list (make it empty initially)
-		List<String> list2Strings = new ArrayList<String>();
-		listView2.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, list2Strings));
-
-		// add an onclicklistener to add an item from the first list to the second list
-		listView1.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView parent, View view, int position, long id) {
-				String item = (String) listView1.getAdapter().getItem(position);
-				if(item != null) {
-					((ArrayAdapter) listView2.getAdapter()).add(item);
-					Toast.makeText(TabbedListListActivity.this, item + " added to list 2", Toast.LENGTH_SHORT).show();
-				}
-			}
-		});
-
-		// add views to tab host
-		tabHost.addTab(tabHost.newTabSpec(LIST1_TAB_TAG).setIndicator(LIST1_TAB_TAG).setContent(new TabContentFactory() {
-			public View createTabContent(String arg0) {
-				return listView1;
-			}
-		}));
-		tabHost.addTab(tabHost.newTabSpec(LIST2_TAB_TAG).setIndicator(LIST2_TAB_TAG).setContent(new TabContentFactory() {
-			public View createTabContent(String arg0) {
-				return listView2;
-			}
-		}));
-		
-		
-		
 		
 		
 	}
