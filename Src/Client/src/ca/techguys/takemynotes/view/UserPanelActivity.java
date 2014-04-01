9dff9dcff7f502c2196cb39d565f216fc5eb94bb
Sheng Nong <shengnong@Tony.local>
2014-04-01 14:36:43 -0400

    Initialize control for user panel activity

--- Src/Client/src/ca/techguys/takemynotes/view/UserPanelActivity.java
+++ Src/Client/src/ca/techguys/takemynotes/view/UserPanelActivity.java
@@ -6,15 +6,69 @@ import ca.techguys.takemynotes.R.menu;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
 
 public class UserPanelActivity extends Activity {
 
+	TextView registerDateTv;
+	TextView loginDateTv;
+	
+	EditText userNameEdt;
+	EditText emailEdt;
+	EditText addressEdt;
+	
+	Button updateBtn;
+	
+	String regDate;
+	String loginDate;
+	String name;
+	String email;
+	String address;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_user_panel);
 		
 		setTitle("My profile");
+		
+		registerDateTv=(TextView) findViewById(R.id.upDateRegisterTv);
+		loginDateTv=(TextView) findViewById(R.id.upLastLoginTv);
+		
+		userNameEdt=(EditText) findViewById(R.id.upNameEdt);
+		emailEdt=(EditText) findViewById(R.id.upEmailEdt);
+		addressEdt=(EditText) findViewById(R.id.upAddressEdt);
+		
+		registerDateTv.setText(regDate);
+		loginDateTv.setText(loginDate);
+		
+		userNameEdt.setText(name);
+		emailEdt.setText(email);
+		addressEdt.setText(address);
+		
+		updateBtn=(Button) findViewById(R.id.upUpdateBtn);
+		updateBtn.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				name=userNameEdt.getText().toString();
+				email=emailEdt.getText().toString();
+				address=addressEdt.getText().toString();
+				
+			}
+			
+		});
+		
+		
+		
+		
+		
 	}
 
 	@Override
