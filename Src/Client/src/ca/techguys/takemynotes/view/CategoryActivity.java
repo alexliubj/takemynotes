0c881b52203ef9c18fb9279aa98a5e95de1c65e5
alex <alexliubo@gmail.com>
2014-04-10 18:20:58 -0400

    update
    update

--- Src/Client/src/ca/techguys/takemynotes/view/CategoryActivity.java
+++ Src/Client/src/ca/techguys/takemynotes/view/CategoryActivity.java
@@ -2,6 +2,8 @@ package ca.techguys.takemynotes.view;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
 import ca.techguys.takemynotes.R;
@@ -13,6 +15,8 @@ import ca.techguys.takemynotes.net.Parse;
 import ca.techguys.takemynotes.net.TakeMyNotesRequest;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -21,6 +25,7 @@ import android.graphics.BitmapFactory;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
@@ -174,70 +179,6 @@ public class CategoryActivity extends Activity {
 		}
 		return super.onKeyDown(keyCode, event);
 	}
-	private class GetData extends AsyncTask<String, String, String> {
-		private Context mContext;
+	
 
-		public GetData(Context context) {
-			this.mContext = context;
-		}
-
-		@Override
-		protected void onPreExecute() {
-			// TODO Auto-generated method stub
-			if (null != dialog && !dialog.isShowing()) {
-				dialog.show();
-			}
-			super.onPreExecute();
-		}
-
-		@Override
-		protected String doInBackground(String... params) {
-			// TODO Auto-generated method stub
-			String result = null;
-			String key = "test string";
-			TakeMyNotesRequest request = new TakeMyNotesRequest(getApplicationContext());
-			try {
-				result = request.getRegisterCaptcha(mobelNo, key);
-				Log.i("zzz", "result = " + result);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (TimeoutException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			return result;
-		}
-
-		@Override
-		protected void onPostExecute(String result) {
-			// TODO Auto-generated method stub
-			if (null != dialog && dialog.isShowing()) {
-				dialog.dismiss();
-			}
-			if (result == null || result.equals("")) {
-				Toast.makeText(CategoryActivity.this, R.string.network_failure, Toast.LENGTH_SHORT)
-						.show();
-			} else {
-				if (new Parse().CommonPares(result).getResult() == 1) {
-					intent = new Intent(CategoryActivity.this, SubCategoryActivity.class);
-					Bundle bundle = new Bundle();
-					bundle.putString("mobelNo", mobelNo);
-					intent.putExtras(bundle);
-					startActivity(intent);
-				} else if (new Parse().CommonPares(result).getResult() == 0) {
-					dialog = new DialogActivity(CategoryActivity.this, 2);
-					dialog.getBtnSure().setOnClickListener(new OnClickListener() {
-						@Override
-						public void onClick(View v) {
-							// TODO Auto-generated method stub
-							dialog.dismiss();
-						}
-					});
-					dialog.setShowMessage(new Parse().CommonPares(result).getErrormessage());
-					dialog.show();
-				}
-			}
-		}
-	}
 }
